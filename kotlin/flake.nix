0e5ff2c866f9ac4b4b49e547320e688375b74be5
Sarthak Gaur <sarthak22gaur@gmail.com>
2025-09-27 03:07:26 -0700

    use lts version

--- kotlin/flake.nix
+++ kotlin/flake.nix
@@ -1,12 +1,10 @@
 {
   description = "A Nix-flake-based Kotlin development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
-      javaVersion = 23; # Change this value to update the whole stack
-
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
@@ -16,7 +14,7 @@
       overlays.default =
         final: prev:
         let
-          jdk = prev."jdk${toString javaVersion}";
+          jdk = prev.jdk;
         in
         {
           gradle = prev.gradle.override { java = jdk; };
