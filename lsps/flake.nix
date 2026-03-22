61d396eff7366aca06729f6a33a09122e170e7a7
Sarthak Gaur <sarthak22gaur@gmail.com>
2026-03-22 03:19:48 -0700

    lsp updates

--- lsps/flake.nix
+++ lsps/flake.nix
@@ -1,58 +1,60 @@
 {
-  description = "A Nix-flake-based LSP support";
+  description = "A Nix-flake-based LSP and formatter development environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
-      {
-        devShells = nixpkgs.lib.genAttrs supportedSystems (system:
-          let
-            pkgs = import nixpkgs { inherit system; };
+    {
+      devShells = forEachSupportedSystem ({ pkgs }:
+        let
+          node-pc = pkgs.writeShellScriptBin "node-pc" ''
+            exec ${pkgs.nodejs-slim_latest}/bin/node --experimental-enable-pointer-compression "$@"
+          '';
 
-            kotlinLspVersion = "0.253.10629";
-            kotlinLspZipUrl = "https://download-cdn.jetbrains.com/kotlin-lsp/${kotlinLspVersion}/kotlin-${kotlinLspVersion}.zip";
-            kotlinLspSha256 = "0zcn89ia6czrcfadjqn51hv4zfq1ax3x3ydw4gc89zrwfjiwc8ic";
+          typescript-language-server-pc = pkgs.writeShellScriptBin "typescript-language-server" ''
+            exec ${pkgs.nodejs-slim_latest}/bin/node --experimental-enable-pointer-compression \
+              ${pkgs.typescript-language-server}/lib/node_modules/typescript-language-server/lib/cli.mjs "$@"
+          '';
 
-            kotlin-lsp = pkgs.stdenv.mkDerivation {
-              pname = "kotlin-lsp";
-              version = kotlinLspVersion;
-              src = pkgs.fetchzip {
-                url = kotlinLspZipUrl;
-                sha256 = kotlinLspSha256;
-                stripRoot = false;
-              };
-              nativeBuildInputs = [ pkgs.unzip pkgs.patchelf ];
-              installPhase = ''
-                mkdir -p $out/bin
-                cp -r * $out/
-                chmod +x $out/kotlin-lsp.sh
-                cat > $out/bin/kotlin-lsp <<EOF
-                #!${pkgs.bash}/bin/bash
-                exec "$out/kotlin-lsp.sh" "\$@"
-                EOF
-                chmod +x $out/bin/kotlin-lsp
-              '';
-            };
-          in
-          {
-            default = pkgs.mkShell {
-              packages = with pkgs; [
-                lua-language-server
-                pyright
-                typescript-language-server
-                rust-analyzer
-                gopls
-                stylua
-                black
-                prettierd
-                kotlin-lsp
-                jdk
-              ];
-            };
-          }
-        );
-      };
+          lsps = with pkgs; [
+            lua-language-server
+            nil
+            pyright
+            typescript-language-server
+            rust-analyzer
+            gopls
+            ruby-lsp
+            yaml-language-server
+            bash-language-server
+            vscode-langservers-extracted
+          ];
+
+          formatters = with pkgs; [
+            stylua
+            black
+            prettierd
+          ];
+
+          commonPackages = [ pkgs.nodejs_latest ] ++ lsps ++ formatters;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = commonPackages;
+          };
+
+          pointer-compression = pkgs.mkShell {
+            packages = [
+              node-pc
+              typescript-language-server-pc
+            ] ++ (pkgs.lib.filter (p: p != pkgs.typescript-language-server) commonPackages);
+          };
+        }
+      );
+    };
 }
